1c171d87cef1264c9460cc94a42d9b3cecb2ebb0
Edrene7 <rene.casillas193@tectijuana.edu.mx>
2019-10-10 09:10:45 -0700

    trabajo numero 1

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+          
+            Console.WriteLine("primer trabajo");
+            int a, b, c, d, e, f;
+            c = a + b;
+            d = a - b;
+            Console.WriteLine("agregar primer numero");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("agregar segundo numero");
+            b = int.Parse(Console.ReadLine());
+            { struct sistem.Bolean
+
+    }
+    }
+}
