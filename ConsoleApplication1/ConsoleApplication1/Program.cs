66b4634c07133b9b449d65463552b9aa80f49fe6
Edrene7 <rene.casillas193@tectijuana.edu.mx>
2019-10-10 09:44:00 -0700

    trabaj 1/5 terminado

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -8,20 +8,32 @@ namespace ConsoleApplication1
 {
     class Program
     {
+        
         static void Main(string[] args)
+          
         {
             Program p = new Program();
-          
+
             Console.WriteLine("primer trabajo");
-            int a, b, c, d, e, f;
-            c = a + b;
-            d = a - b;
+            int a, b, w, x, y, z;
             Console.WriteLine("agregar primer numero");
             a = int.Parse(Console.ReadLine());
             Console.WriteLine("agregar segundo numero");
             b = int.Parse(Console.ReadLine());
-            { struct sistem.Bolean
-
-    }
+            w = a + b;
+            x = a - b;
+            y = b / a;
+            z = a * b;
+            Console.WriteLine("suma");
+            if (a > b) Console.WriteLine(w);
+            Console.WriteLine("diferencia");
+            if (a > b) Console.WriteLine(x);
+            Console.WriteLine("producto");
+            if (a < b) Console.WriteLine(z);
+            Console.WriteLine("division");
+            if (a < b) Console.WriteLine(y);
+            Console.ReadKey();
+        }
+       
     }
 }
